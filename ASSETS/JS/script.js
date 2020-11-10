38fe0ec6da0d9d85eab62272bdbb2ef7164c703f
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-10 15:53:52 +0100

    modification du 10.11.20

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,40 @@
+
+// // page contact - changement de couleurs des images
+// document.getElementById('mailImg').onmouseover = function mouseover(){
+    
+//             document.getElementById('mailImg').src="ASSETS/IMG/mail2.png";
+//     };
+//     document.getElementById('mailImg').onmouseout = function outmouseover(){
+
+//             document.getElementById('mailImg').src="ASSETS/IMG/mail.png";
+//     };
+//     document.getElementById('linkedinImg').onmouseover = function mouseover(){
+    
+//         document.getElementById('linkedinImg').src="ASSETS/IMG/linkedin2.png";
+//     };
+//     document.getElementById('linkedinImg').onmouseout = function outmouseover(){
+
+//         document.getElementById('linkedinImg').src="ASSETS/IMG/linkedin.png";
+//     };
+//     document.getElementById('behanceImg').onmouseover = function mouseover(){
+    
+//         document.getElementById('behanceImg').src="ASSETS/IMG/behance2.png";
+//     };
+//     document.getElementById('behanceImg').onmouseout = function outmouseover(){
+        
+//         document.getElementById('behanceImg').src="ASSETS/IMG/behance.png";
+//     };
+// document.getElementById('instaImg').onmouseover = function mouseover(){
+    
+//         document.getElementById('instaImg').src="ASSETS/IMG/insta2.png";
+//     };
+//  document.getElementById('instaImg').onmouseout = function outmouseover(){
+        
+//  document.getElementById('instaImg').src="ASSETS/IMG/insta.png";
+//     };
+
+
+  $(function() {
+    $( "#accordion" ).accordion();
+});
+
